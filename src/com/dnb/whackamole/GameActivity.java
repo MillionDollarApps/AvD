a15cd1260e19b9229efb75593272d812dd6febb1
stefandragosnitu <nt_stefan@yahoo.com>
2014-11-01 21:03:16 +0200

    added main window for game activity

--- src/com/dnb/whackamole/GameActivity.java
+++ src/com/dnb/whackamole/GameActivity.java
@@ -2,12 +2,22 @@ package com.dnb.whackamole;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class GameActivity extends Activity {
+
+    private ImageView bg1;
+    private ImageView bg2;
+    private ImageView bg3;
+    private ImageView bg4;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        bg1 = (ImageView) findViewById(R.id.bg1);
+        bg2 = (ImageView) findViewById(R.id.bg2);
+        bg3 = (ImageView) findViewById(R.id.bg3);
+        bg4 = (ImageView) findViewById(R.id.bg4);
     }
 }
